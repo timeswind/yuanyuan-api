4df26009c0c93efa8bd114e8bfe3979b8bbf2652
Mingtian Yang <tswymt@gmail.com>
2017-12-08 20:44:19 -0500

    update

--- routes/protect/profile/avatar.js
+++ routes/protect/profile/avatar.js
@@ -3,6 +3,7 @@ var $User = Models.$User;
 
 exports.post = function* () {
   var newAvatarUrl = this.request.body.url
+  console.log(this.request.body)
   var user_id = this.state.user.id
 
   var patchedUser = $User.patch(user_id, 'avatar', newAvatarUrl)
